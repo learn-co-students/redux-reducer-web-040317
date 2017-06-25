45913a4aadcf87eb9f0b2ba24f2dc35067ae2251
Dan Parry <parryd424@gmail.com>
2017-06-25 12:44:56 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      state.friends.push(action.friend)
+      return state
+    case 'REMOVE_FRIEND':
+      let newState = {}
+      newState.friends = state.friends.filter(f => f.id !== action.id)
+      return newState
+    default:
+      return state
+  }
 }
+
+// pretty sure this wasn't the intended solution. 
