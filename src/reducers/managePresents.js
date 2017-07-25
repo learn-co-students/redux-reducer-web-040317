4c2f1c4a3471e7a01dbed88af5f9b276524a73c8
github username <github email address>
2017-07-25 14:00:32 -0400

    presents function complete

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,5 +1,15 @@
 export function managePresents(state = {
   presents: 0,
 }, action){
+  switch(action.type){
+
+    case "increase":
+    return Object.assign({}, state, {
+      presents: state.presents += 1
+    })
+
+    default:
+    return state
+  }
 
 }
